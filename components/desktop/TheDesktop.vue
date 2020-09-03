fec91521e9ddaa82fec2e96de5abee96602350ba
Hamza <hamzabitup@gmail.com>
2020-09-03 19:34:23 +0100

    Add: desktop

---
+++ components/desktop/TheDesktop.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="the-desktop relative bg-center bg-cover"></div>
+</template>
+
+<script>
+export default {
+  name: "TheDesktop"
+};
+</script>
+
+<style scoped>
+.the-desktop {
+  flex: 1;
+  background-image: url('/imgs/desktop.jpeg');
+}
+</style>
---
+++ components/desktop/folders/DesktopFoldersIcon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DesktopFolderIcon"
+};
+</script>
+
+<style lang="scss" scoped></style>
