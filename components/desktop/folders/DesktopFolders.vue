514b742d6959e93a0d4bbeaa05364e2e679c7904
Hamza <hamzabitup@gmail.com>
2020-09-04 21:07:51 +0100

    Add: desktop folders

--- components/desktop/folders/DesktopFolders.vue
+++ components/desktop/folders/DesktopFolders.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <desktop-folders-element />
-  </div>
+  <ul class="flex flex-wrap w-full max-w-md absolute top-0 left-0 z-0 mt-6">
+    <li v-for="i in 6" :key="i" class="p-2">
+      <desktop-folders-element :icon="'/imgs/directory_closed-4.png'" name="About"/>
+    </li>
+  </ul>
 </template>
 
 <script>
--- components/desktop/folders/DesktopFoldersElement.vue
+++ components/desktop/folders/DesktopFoldersElement.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>
-    
+  <div
+    class="flex flex-col items-center text-center cursor-pointer"
+    style="width: 76px; height: 70px;"
+  >
+    <!-- icon  -->
+    <div class="flex justify-center items-center mb-2">
+      <img :src="icon" :alt="name" style="max-height: 22px; max-width: 32px;" />
+    </div>
+    <!-- text  -->
+    <span class="folder-text">
+      {{ name }}
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-  name: "DesktopFolderIcon"
+  name: "DesktopFolderIcon",
+
+  props: {
+    icon: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.folder-text {
+  padding: 1px 3px;
+  background: rgb(216, 215, 231);
+  border: 1px solid #effbfe;
+  border-color: rgb(216, 215, 231);
+  font-family: Arial;
+  font-size: 11px;
+  line-height: 1;
+}
+</style>
