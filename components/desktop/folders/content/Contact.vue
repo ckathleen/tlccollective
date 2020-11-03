9c7d4f23f83a376a7f7ef1bdad97ae0e63d5db92
lstephanian <stephanianlm@gmail.com>
2020-11-03 13:55:12 -0800

    adding better content to about and contact

--- components/desktop/folders/content/Contact.vue
+++ components/desktop/folders/content/Contact.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="about">
-        Todo: create contact form or link directly to users email
+        If you would like to pitch TLC, are interested in building and/or investing with us, or are have general questions, please send us a note <u><a href="mailto:tlcinvestments@googlegroups.com">here</a></u>.
+        <br/>
+        <br/>
+        Alternatively, you can always send us a DM:<br/>
+        --> <u><a href="https://twitter.com/tlc_collective" target="_blank">TLC Team</a></u><br/>
+        --> <u><a href="https://twitter.com/tcburning" target="_blank">Terri Burns</a></u><br/>
+        --> <u><a href="https://twitter.com/lstephanian" target="_blank">Lauren Stephanian</a></u><br/>
+        --> <u><a href="https://twitter.com/caseykcaruso" target="_blank">Casey Caruso</a></u>
     </div>
 </template>
 
