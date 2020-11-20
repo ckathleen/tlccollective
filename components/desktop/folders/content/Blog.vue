602c6311d4edf4cdbb315b41eeed18e2b59d0e2e
lstephanian <stephanianlm@gmail.com>
2020-11-20 08:39:13 -0800

    some slight progress with iframe sizing

--- components/desktop/folders/content/Blog.vue
+++ components/desktop/folders/content/Blog.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <iframe
+    <!--<iframe
       src="https://docs.google.com/document/d/e/2PACX-1vQjVGD02qHS5z1avA_SAsxWGpjePRg_1TOzzI6QRkZlHmun2iX3xMjl5yJjB0a7e96lor-ReqWEN9fx/pub?embedded=true"
-      width=100%
-      height=100%
+      width=100vw
+      height=100vh
       position=absolute
       border=none
-    ></iframe>
+    ></iframe>-->
+
+    <!--<iframe
+    <object data="https://docs.google.com/document/d/e/2PACX-1vQjVGD02qHS5z1avA_SAsxWGpjePRg_1TOzzI6QRkZlHmun2iX3xMjl5yJjB0a7e96lor-ReqWEN9fx/pub?embedded=true" width="600vw" height="400vh">
+    <embed src="https://docs.google.com/document/d/e/2PACX-1vQjVGD02qHS5z1avA_SAsxWGpjePRg_1TOzzI6QRkZlHmun2iX3xMjl5yJjB0a7e96lor-ReqWEN9fx/pub?embedded=true" width="600vw" height="400vh"> </embed>
+    Error: Embedded data could not be displayed.
+    </object>-->
+    
+    <div class="resp-container">
+      <iframe class="resp-iframe" src="https://docs.google.com/document/d/e/2PACX-1vQjVGD02qHS5z1avA_SAsxWGpjePRg_1TOzzI6QRkZlHmun2iX3xMjl5yJjB0a7e96lor-ReqWEN9fx/pub?embedded=true" gesture="media"  allow="encrypted-media" allowfullscreen></iframe>
+    </div>
+    
   </div>
 </template>
 
@@ -15,3 +26,20 @@ export default {
   name: 'FolderContentBlog'
 }
 </script>
+
+<style scoped>
+
+.resp-container {
+    position: relative;
+    overflow: hidden;
+    padding-top: 85.71%;
+}
+.resp-iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 120%;
+    border: 0;
+}
+</style>
