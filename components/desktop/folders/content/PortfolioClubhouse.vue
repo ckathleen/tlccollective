14436ac2e1f5f8fe3b9d6bda2a7894bf12d19e80
lstephanian <stephanianlm@gmail.com>
2020-11-23 13:19:48 -0500

    tempdeploy pre-memo approval

--- components/desktop/folders/content/PortfolioClubhouse.vue
+++ components/desktop/folders/content/PortfolioClubhouse.vue
@@ -6,7 +6,15 @@
       and
       height="360"
     ></iframe>-->
-    We don't have a public  memo available for this investment! But check out <u><a href=#>this</a></u> dreamy music vid.
+    We don't have a public  memo available for this investment.
+    <br/><br/>
+    Clubhouse in the news:
+    <br/>
+    <ul>
+      <li>--> <u><a href="https://www.cnbc.com/2020/05/20/clubhouse-app-is-where-mc-hammer-and-jared-leto-chat-with-vcs.html" target="_blank">Silicon Valley is going crazy for Clubhouse</a></u></li>
+      <li>--> <u><a href="https://www.nytimes.com/2020/05/19/technology/clubby-silicon-valley-app-clubhouse.html" target="_blank">The Hot New Thing in Clubby Silicon Valley? An App Called Clubhouse</a></u></li>
+      <li>--> <u><a href="https://www.wired.com/story/what-is-clubhouse-why-does-silicon-valley-care/" target="_blank">What Is Clubhouse, and Why Does Silicon Valley Care?</a></u></li>
+    </ul>
   </div>
 </template>
 
--- components/desktop/folders/content/PortfolioZeta.vue
+++ components/desktop/folders/content/PortfolioZeta.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <div class="resp-container">
+    More info on Zeta coming soon.
+        <br/><br/>
+    Zeta in the news:
+    <br/>
+    <ul>
+      <li>--> <u><a href="https://www.bankrate.com/banking/best-budgeting-apps/" target="_blank">The best budgeting apps to make your smartphone your wallet’s best friend</a></u></li>
+      <li>--> <u><a href="https://www.refinery29.com/en-us/who-should-pay-for-birth-control" target="_blank">Should Couples Share The Cost Of Birth Control? That Depends On These Factors</a></u></li>
+      <li>--> <u><a href="https://www.pcmag.com/how-to/feeling-money-stress-follow-our-8-steps-for-beating-budget-burnout" target="_blank">Feeling Money Stress? Follow Our 8 Steps for Beating Budget Burnout</a></u></li>
+      <li>--> <u><a href="https://loans.usnews.com/articles/how-to-teach-your-college-age-kids-about-debt" target="_blank">How to Teach Your College-Age Kids About Debt</a></u></li>
+
+    </ul>
+    <!--<div class="resp-container">
       <iframe class="resp-iframe" src="https://docs.google.com/document/d/e/2PACX-1vRqKCER5hBR7RsMEJuPqi0msXISZmTJlvoDPbDSOeA6bXRf4EDMHF5E42s4ErOhfP-zTzz2QW7JKLd1/pub?embedded=true" gesture="media"  allow="encrypted-media" allowfullscreen></iframe>
-    </div>
+    </div>-->
     
   </div>
 </template>
