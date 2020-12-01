7f96b8cd672aecc0f636ec276a2f1ab560bb94b0
Terri Burns <tcburning@gmail.com>
2020-11-30 23:06:10 -0800

    Quick sizing and spacing fixes

--- components/desktop/folders/content/PortfolioClubhouse.vue
+++ components/desktop/folders/content/PortfolioClubhouse.vue
@@ -7,14 +7,6 @@
     Lauren and Casey invested in this company in summer 2020.
     <br><br>
     <b>CLUBHOUSE IN THE NEWS</b><br>
-    <!--<iframe
-      src="https://docs.google.com/document/d/e/2PACX-1vR3flcVH0GDEJE4U7NonIz63gMHDBoKGNf9zqgN5yjfyOcJ6ONALFhScfEQybZyGp7FUxx96xxwnH4h/pub?embedded=true"
-      width="487"
-      and
-      height="360"
-    ></iframe>-->
-    <br/><br/>
-    <br/>
     <ul>
       <li>--> <u><a href="https://www.cnbc.com/2020/05/20/clubhouse-app-is-where-mc-hammer-and-jared-leto-chat-with-vcs.html" target="_blank">Silicon Valley is going crazy for Clubhouse</a></u></li>
       <li>--> <u><a href="https://www.nytimes.com/2020/05/19/technology/clubby-silicon-valley-app-clubhouse.html" target="_blank">The Hot New Thing in Clubby Silicon Valley? An App Called Clubhouse</a></u></li>
